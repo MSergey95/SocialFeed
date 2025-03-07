e7f75e22484a4018c5344c8439d6bdd892e009dc
Sergey <121692119+MSergey95@users.noreply.github.com>
2025-03-07 09:50:23 +0800

    Initial Commit

---
+++ SocialFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialFeed
+//
+//  Created by Сергей Минеев on 3/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
